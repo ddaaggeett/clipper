e60ecba469342ceb192b18a6035ea9a65aad5c96
ddaaggeett <david.g.daggett@gmail.com>
2020-12-28 15:05:23 -0500

    init data object in server on startup

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,27 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var { port } = require('../../config')
 var handleClip = require('./handleClip')
+var fs = require('fs')
+
+const dataFile = __dirname + "/data.json"
+var data
+fs.readFile(dataFile, (error, jsonString) => {
+    if (!error) {
+        if (jsonString) {
+            // console.log(JSON.parse(jsonString))
+            data = JSON.parse(jsonString)
+        }
+    }
+    else {
+        if ( error.code === "ENOENT" ) { // file doesn't exist
+            console.log("ERROR\n",error)
+            fs.writeFile(dataFile, JSON.stringify({}), (error) => {
+                if(error) console.log(error)
+                else console.log("file created @ ", dataFile)
+            })
+        }
+    }
+})
 
 io.on('connection', (socket) => {
     socket.on('clip', (clipObject, confirmReceived) => {
