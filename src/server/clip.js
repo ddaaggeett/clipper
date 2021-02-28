37384abff2401cf5b9181a06daddf368534762c2
ddaaggeett <david.g.daggett@gmail.com>
2021-02-27 23:28:49 -0500

    user may edit clip data before source video download completes. use latest stored object from DB to generate new clip

--- src/server/clip.js
+++ src/server/clip.js
@@ -1,26 +1,36 @@
-const {
-    exec,
-} = require('child_process')
+const { exec } = require('child_process')
 const fs = require('fs')
+var { dbConnxConfig } = require('../../config')
+var r = require('rethinkdb')
 
 const clip = (videoDirectory, clipObject) => {
-    // TODO: generate better unique clip ID
-    let clipID
-    if(clipObject.title.length == 0) clipID = clipObject.timestamp + ".mp4"
-    else {
-        const filenameString = clipObject.title.split(' ').join('_')
-        clipID = clipObject.timestamp + '_' + filenameString + '.mp4'
-    }
-    const command = "ffmpeg -ss " + clipObject.start + " -i *" + clipObject.videoId + ".mp4 -t " + clipObject.duration + " -c copy " + clipID
-    exec(command, {
-        cwd: videoDirectory,
-    }, (error, stdout, stderr) => {
-        if (error) {
-            console.error('ERROR clip\n', error)
-            return
-        }
-        console.log('clip made @ ', videoDirectory + "/" + clipID)
+
+    r.connect(dbConnxConfig).then(connection => {
+        r.table('clips').get(clipObject.id).run(connection).then(storedClip => { // clip may have been edited before file finished downloading, so get what was sstoredstored
+            // TODO: generate better unique clip ID
+            let clipID
+            if(storedClip.title.length == 0) clipID = storedClip.timestamp + ".mp4"
+            else {
+                const filenameString = storedClip.title.split(' ').join('_')
+                clipID = storedClip.timestamp + '_' + filenameString + '.mp4'
+            }
+            const command = "ffmpeg -ss " + storedClip.start + " -i *" + storedClip.videoId + ".mp4 -t " + storedClip.duration + " -c copy " + clipID
+            exec(command, {
+                cwd: videoDirectory,
+            }, (error, stdout, stderr) => {
+                if (error) {
+                    console.error('ERROR clip\n', error)
+                    return
+                }
+                console.log('clip made @ ', videoDirectory + "/" + clipID)
+            })
+        }).error(error => {
+            console.log(`\nget clip object error\n${error}`)
+        })
+    }).error(error => {
+        console.log(`\ndb connection error\n${error}`)
     })
+
 }
 
 module.exports = clip
