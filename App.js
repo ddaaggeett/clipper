0d614f66405ee90f0b1a333a3e55b24fb81501b2
ddaaggeett <david.g.daggett@gmail.com>
2021-04-13 09:43:34 -0400

    get rid of next.js. TODO: server should be only express.js

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import AppMain from './src/app'
+import WebApp from './src/app/web'
 import { styles } from "./src/app/styles"
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import configureStore from './src/app/redux';
+import configureStoreWeb from './src/app/redux/web';
 
 const store = configureStore().store
 const persistor = configureStore().persistor
+const storeWeb = configureStoreWeb().store
+const persistorWeb = configureStoreWeb().persistor
 
 export default function App() {
-    return (
+    if (Platform.OS === 'web') return (
+        <Provider store={storeWeb}>
+        <PersistGate loading={null} persistor={persistorWeb}>
+            <WebApp />
+        </PersistGate>
+        </Provider>
+    )
+    else return (
         <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer>
--- pages/_document.js
+++
@@ -1,2 +0,0 @@
-// @generated: @expo/next-adapter@2.1.57
-export { default } from '@expo/next-adapter/document';
