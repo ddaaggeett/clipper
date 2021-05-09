807e2a422e105edc7db204d3515d7b0eafabed45
ddaaggeett <david.g.daggett@gmail.com>
2021-05-08 22:17:24 -0400

    sockets own file

--- src/server/index.js
+++ src/server/index.js
@@ -1,38 +1,3 @@
-var express = require('express')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http, { cors: { origin: "*", methods: ["GET", "POST"] } })
-var { socketPort } = require('../../config')
-var { updateClip, deleteClip, handlePendingClips } = require('./handleReceiveClips')
-var { userLog, getUserClips } = require('./user')
-var generateClip = require('./generateClip')
 require('./db')
 require('./expressServer')
-
-io.on('connection', (socket) => {
-    socket.on('reClip', clipObject => generateClip(clipObject))
-    socket.on('updateClip', (clip, returnToSender) => {
-        updateClip(clip).then(updatedClip => {
-            returnToSender(updatedClip)
-            socket.broadcast.emit('updateClip',updatedClip)
-        })
-    })
-    socket.on('deleteClip', (clip, returnToSender) => {
-        deleteClip(clip).then(deletedClip => {
-            returnToSender(deletedClip)
-            socket.broadcast.emit('deleteClip',deletedClip)
-        })
-    })
-    socket.on('userLog', (user, sendBack) => {
-        userLog(user).then(userData => sendBack(userData))
-    })
-    socket.on('getUserClips', ({user_id, pendingClips}, sendBack) => {
-        handlePendingClips(pendingClips).then(() => {
-            getUserClips(user_id).then(userClips => sendBack(userClips))
-        })
-    })
-})
-
-http.listen(socketPort, function(){
-    console.log('socket.io listening on *:' + socketPort)
-})
+require('./sockets')
---
+++ src/server/sockets.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http, { cors: { origin: "*", methods: ["GET", "POST"] } })
+var { socketPort } = require('../../config')
+var { updateClip, deleteClip, handlePendingClips } = require('./handleReceiveClips')
+var { userLog, getUserClips } = require('./user')
+var generateClip = require('./generateClip')
+
+io.on('connection', (socket) => {
+    socket.on('reClip', clipObject => generateClip(clipObject))
+    socket.on('updateClip', (clip, returnToSender) => {
+        updateClip(clip).then(updatedClip => {
+            returnToSender(updatedClip)
+            socket.broadcast.emit('updateClip',updatedClip)
+        })
+    })
+    socket.on('deleteClip', (clip, returnToSender) => {
+        deleteClip(clip).then(deletedClip => {
+            returnToSender(deletedClip)
+            socket.broadcast.emit('deleteClip',deletedClip)
+        })
+    })
+    socket.on('userLog', (user, sendBack) => {
+        userLog(user).then(userData => sendBack(userData))
+    })
+    socket.on('getUserClips', ({user_id, pendingClips}, sendBack) => {
+        handlePendingClips(pendingClips).then(() => {
+            getUserClips(user_id).then(userClips => sendBack(userClips))
+        })
+    })
+})
+
+http.listen(socketPort, function(){
+    console.log('socket.io listening on *:' + socketPort)
+})
