19a07344befededed501b697f7e822b653592870
ddaaggeett <david.g.daggett@gmail.com>
2022-07-18 10:57:57 -0400

    clipper app further separation

--- src/app/clipper/index.js
+++ src/app/clipper/index.js
@@ -1,13 +1,43 @@
+import { useEffect } from 'react'
+import { Platform, Dimensions } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { serverIP, socketPort } from '../../../config'
 import ClipperScreen from './ClipperScreen'
 import ClipManagerScreen from './ClipManagerScreen'
 import useDataSocketHook from './dataSocket'
+import * as actions from './redux/actions/actionCreators'
+import { io } from 'socket.io-client'
+
+const socket = io('http://'+ serverIP + ':' + socketPort.clipper)
 
 const Tab = createBottomTabNavigator()
 
 export default () => {
 
     useDataSocketHook()
+
+    const redux = useDispatch()
+    const { loggedIn, user } = useSelector(state => state.account)
+    const { pending } = useSelector(state => state.clips)
+
+    useEffect(() => {
+        if (Platform.OS === 'web') {
+            const width = Dimensions.get('window').width / 2
+            if (width > 640) redux(actions.setWebPanelWidth(640))
+            else redux(actions.setWebPanelWidth(width))
+        }
+        if (loggedIn && user !== null) {
+            const packet = {
+                userID: user.id,
+                pendingClips: pending,
+            }
+            socket.emit('getUserClips', packet, userClips => {
+                redux(actions.clearPending())
+                redux(actions.updateClips(userClips))
+            })
+        }
+    }, [])
 
     const tabBarOptions = {
         tabBarActiveBackgroundColor: '#222',
--- src/app/index.js
+++ src/app/index.js
@@ -7,37 +7,10 @@ import Drawer_Whitesocket from './whitesocket'
 import WebApp from './web'
 import { styles } from './clipper/styles'
 import { createDrawerNavigator } from '@react-navigation/drawer'
-import { useSelector, useDispatch } from 'react-redux'
-import * as actions from './clipper/redux/actions/actionCreators'
-import { serverIP, socketPort } from '../../config'
-import { io } from 'socket.io-client'
-const socket = io('http://'+ serverIP + ':' + socketPort)
 
 const Drawer = createDrawerNavigator()
 
 export default () => {
-
-    const { loggedIn, user } = useSelector(state => state.account)
-    const redux = useDispatch()
-    const { clips, pending } = useSelector(state => state.clips)
-
-    useEffect(() => {
-        if (Platform.OS === 'web') {
-            const width = Dimensions.get('window').width / 2
-            if (width > 640) redux(actions.setWebPanelWidth(640))
-            else redux(actions.setWebPanelWidth(width))
-        }
-        if (loggedIn && user !== null) {
-            const packet = {
-                userID: user.id,
-                pendingClips: pending,
-            }
-            socket.emit('getUserClips', packet, userClips => {
-                redux(actions.clearPending())
-                redux(actions.updateClips(userClips))
-            })
-        }
-    }, [])
 
     if (Platform.OS === 'web') {
         return (
