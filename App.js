d671835cd654219234866eb0b8217ddb5c8510ce
ddaaggeett <david.g.daggett@gmail.com>
2022-07-19 13:59:46 -0400

    configure linking

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
+import * as Linking from 'expo-linking'
 import React from 'react';
 import { View, Platform } from 'react-native';
 import AppMain from './src/app'
@@ -13,11 +14,25 @@ const persistor = configureStore().persistor
 const storeWeb = configureStoreWeb().store
 const persistorWeb = configureStoreWeb().persistor
 
+const linking = {
+    prefixes: [
+        Linking.createURL('/'),
+        'https://ddaaggeett.xyz',
+        'https://*.ddaaggeett.xyz',
+        'ddaaggeett://',
+    ],
+    config: {
+        screens: {
+        },
+    }
+
 export default function App() {
     if (Platform.OS === 'web') return (
         <Provider store={storeWeb}>
         <PersistGate loading={null} persistor={persistorWeb}>
+        <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
             <AppMain />
+        </NavigationContainer>
         </PersistGate>
         </Provider>
     )
