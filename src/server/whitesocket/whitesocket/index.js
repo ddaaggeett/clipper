48333b15a2a6447a65b18064f8004042ea65e9ea
ddaaggeett <david.g.daggett@gmail.com>
2022-10-04 23:35:13 -0400

    nodemon ignore files reduction + defaultDiff out of config file

--- src/server/whitesocket/whitesocket/index.js
+++ src/server/whitesocket/whitesocket/index.js
@@ -1,8 +1,18 @@
 const { exec } = require('child_process')
 const path = require('path')
-const config = require('../../../../config')
 
-const whitesocket = (diff = config.defaultDiff) => {
+const defaultDiff =  {
+    mode: "write",
+    shape: {
+        "height": 600,
+        "width": 800
+    },
+    prev_uri: "blank.jpg",
+    result_uri: "result.jpg",
+    uri: "image.jpg",
+}
+
+const whitesocket = (diff = defaultDiff) => {
     return new Promise(function(resolve, reject) {
         exec(`python ./src/server/whitesocket/index.py '${JSON.stringify(diff)}'`, (error, stdout, stderr) => {
             if(!error) {
